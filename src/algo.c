4b3fe93da1cc81999a78e1e3087064766f4cf8d5
Cristopher Hermiston <chermist@at-o7.21-school.ru>
2019-09-17 23:29:11 +0300

    implement bfs and queue

--- src/algo.c
+++ src/algo.c
@@ -6,15 +6,30 @@
 /*   By: chermist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 16:48:29 by chermist          #+#    #+#             */
-/*   Updated: 2019/09/16 21:11:52 by chermist         ###   ########.fr       */
+/*   Updated: 2019/09/17 23:17:36 by chermist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+void	bfs(int	i, t_support *sup)
+{
+	t_vec	*queue;
+
+}
+
 void	path_find(t_support *sup)
 {
+	t_lem	*room;
 	t_vec	*shortest;
+	int		i;
 
-	shortest()
+	i = -1;
+	while (++i < sup->farm->size)
+	{
+		room = *(t_lem**)ft_vat(sup->farm, i);
+		if (room->room_status == 1)
+			break;
+	}
+	bfs(i, sup);
 }
