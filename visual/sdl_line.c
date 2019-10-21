17b2f0a9bdf8bac7a8adc214cfad20caca71c168
Cristopher Hermiston <chermist@at-o7.21-school.ru>
2019-10-22 01:42:46 +0300

    sdl visual

---
+++ visual/sdl_line.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sdl_line.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chermist <chermist@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/22 01:07:24 by chermist          #+#    #+#             */
+/*   Updated: 2019/10/22 01:42:12 by chermist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include <SDL.h>
+#include <SDL2_gfxPrimitives.h>
+
+#define WINDOW_WIDTH 600
+
+int main(void) {
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    int i;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_WIDTH, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    for (i = 0; i < WINDOW_WIDTH; ++i)
+        SDL_RenderDrawPoint(renderer, i, i);
+	SDL_RenderDrawLine(renderer, 50, 300, 450, 200);
+	thickLineRGBA(renderer, 100, 50, 500, 550, 5, 255, 255, 255, 50);
+	filledCircleRGBA(renderer, 40, 40, 30, 200, 200, 200,
+							SDL_ALPHA_OPAQUE);
+//	filledPieRGBA (renderer, 400, 100, 30, 10, 20, 200, 200, 200, SDL_ALPHA_OPAQUE);
+    SDL_RenderPresent(renderer);
+    while (1) {
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
